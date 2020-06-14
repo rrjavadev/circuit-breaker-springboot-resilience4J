75b28d0c73832943a8fbc55e2410a4dfdf79e939
roshinivr <roshinirajesh1@gmail.com>
2020-06-14 23:28:24 +0100

    Added circuit breaker to service api call.

--- client-api/src/main/java/org/example/client/external/LocationExternalServiceImpl.java
+++ client-api/src/main/java/org/example/client/external/LocationExternalServiceImpl.java
@@ -1,25 +1,35 @@
 package org.example.client.external;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class LocationExternalServiceImpl implements LocationExternalService {
 
     private final RestTemplate restTemplate;
+    private CircuitBreakerFactory circuitBreakerFactory;
 
-    public LocationExternalServiceImpl(RestTemplate restTemplate) {
+    public LocationExternalServiceImpl(RestTemplate restTemplate,
+                                       CircuitBreakerFactory circuitBreakerFactory) {
         this.restTemplate = restTemplate;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @Override
     public List<String> getLocations() {
-        return restTemplate.exchange("http://localhost:8082/server-api/v1/locations",
+
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("locationServiceCircuitbreaker");
+        return circuitBreaker.run(() -> restTemplate.exchange("http://localhost:8082/server-api/v1/locations",
                 HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
-                }).getBody();
+                }), throwable -> ResponseEntity.accepted()
+                .body(Collections.singletonList("default location"))).getBody();
     }
 }
